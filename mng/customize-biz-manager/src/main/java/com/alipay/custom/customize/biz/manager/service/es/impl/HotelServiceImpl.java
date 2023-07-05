9ab6a0a2d92151ba63695e86f0cfc977918b0fd4
锐途 <ruitu.xr@antgroup.com>
2023-07-05 17:20:57 +0800

    fix issue

--- mng/customize-biz-manager/src/main/java/com/alipay/custom/customize/biz/manager/service/es/impl/HotelServiceImpl.java
+++ mng/customize-biz-manager/src/main/java/com/alipay/custom/customize/biz/manager/service/es/impl/HotelServiceImpl.java
@@ -11,10 +11,16 @@ import com.alipay.custom.customize.common.base.web.PageResult;
 import com.alipay.custom.customize.common.base.web.RequestParams;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -32,17 +38,36 @@ public class HotelServiceImpl implements HotelService {
 
     @Override
     public PageResult<List<HotelDTO>> search(RequestParams params) {
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+
         //构建query查询条件
         BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
         buildBasicQuery(params, boolQuery);
+        nativeSearchQueryBuilder.withQuery(boolQuery);
 
-        NativeSearchQuery query = new NativeSearchQuery(boolQuery);
+        //算分控制
+        FunctionScoreQueryBuilder queryBuilder = QueryBuilders.functionScoreQuery(
+                //原始查询，相关性算分的查询
+                boolQuery,
+                //function score的数组
+                new FunctionScoreQueryBuilder.FilterFunctionBuilder[]{
+                        //其中的一个function score 元素
+                        new FunctionScoreQueryBuilder.FilterFunctionBuilder(
+                                //过滤条件
+                                QueryBuilders.termQuery("isAd", true),
+                                //算分函数
+                                ScoreFunctionBuilders.weightFactorFunction(10))
+                });
+        nativeSearchQueryBuilder.withQuery(queryBuilder);
+        //数据聚合
+        TermsAggregationBuilder aggregationBuilder = AggregationBuilders.terms("agg_name").field("brand").size(10);
+        nativeSearchQueryBuilder.addAggregation(aggregationBuilder);
 
         //分页
-        query.setPageable(PageRequest.of(params.getPage(), params.getSize()));
+        nativeSearchQueryBuilder.withPageable(PageRequest.of(params.getPage(), params.getSize()));
 
         //查询es
-        SearchHits<HotelDTO> searchHits = elasticsearchRestTemplate.search(query, HotelDTO.class);
+        SearchHits<HotelDTO> searchHits = elasticsearchRestTemplate.search(nativeSearchQueryBuilder.build(), HotelDTO.class);
 
         //获取命中总数
         long totalHits = searchHits.getTotalHits();
@@ -53,6 +78,15 @@ public class HotelServiceImpl implements HotelService {
             HotelDTO content = hotelDTOSearchHit.getContent();
             hotelDTOS.add(content);
         }));
+
+        //获取聚合结果
+        Aggregations aggregations = (Aggregations) searchHits.getAggregations();
+        Terms terms = (Terms) aggregations.asMap().get("brand");
+        for (Terms.Bucket bucket : terms.getBuckets()) {
+            String keyAsString = bucket.getKeyAsString(); //聚合字段列的值
+            long docCount = bucket.getDocCount(); //聚合字段对应的数量
+            System.out.println(keyAsString + " " + docCount);
+        }
 
         return new PageResult<>(totalHits, hotelDTOS);
     }
